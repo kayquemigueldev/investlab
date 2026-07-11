bbb731ed5b6651e9b83354960b8595ae23111f66
kayquemigueldev <kayquemiguel.dev@gmail.com>
2026-07-11 12:41:03 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/kayque/investlab/InvestlabApplication.java
@@ -0,0 +1,13 @@
+package com.kayque.investlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestlabApplication.class, args);
+    }
+
+}
